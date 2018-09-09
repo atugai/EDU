72e50a5ce0beee5e3851fa00bdfc887473a2025b
atugai <43098955+atugai@users.noreply.github.com>
2018-09-09 12:37:15 +0100

    [hackerrank.com] Kangaroo excercise tests

---
+++ GO/HACKERRANK/kangaroo_test.go
@@ -0,0 +1,42 @@
+package kangaroo
+
+import (
+  "testing"
+)
+
+func TestKangaroo(t *testing.T) {
+  tests := []struct{
+    desc string
+    x1, v1, x2, v2 int 
+    want bool
+  }{  
+    {   
+      desc: "kangaroo 1 meets 2", 
+      x1: 0,
+      v1: 3,
+      x2: 4,
+      v2: 2,
+      want: true,
+    }, {
+      desc: "kangaroo 1 meets 2", 
+      x1: 2,
+      v1: 1,
+      x2: 1,
+      v2: 2,
+      want: true,
+    }, {
+      desc: "kangaroo 1 doesn't meet 2", 
+      x1: 0,
+      v1: 2,
+      x2: 5,
+      v2: 3,
+      want: false,
+    },  
+  }   
+
+  for i, tc := range tests {
+    if got := Kangaroo(tc.x1, tc.v1, tc.x2, tc.v2); got != tc.want {
+      t.Errorf("TestKangaroo(%d): %s: unexpected result got: %v, want: %v", i, tc.desc, got, tc.want)
+     }   
+   }   
+}
