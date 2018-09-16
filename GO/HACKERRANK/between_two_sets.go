131c105ffb5bb3b0d05fcd66d42185ad58ecb206
atugai <anton.tugai@gmail.com>
2018-09-16 23:11:08 +0100

    [Go][hackerrank.com] Between Two Sets excercise with tests

---
+++ GO/HACKERRANK/between_two_sets.go
@@ -0,0 +1,46 @@
+/*
+
+[hackerrank.com] Between Two Sets
+https://www.hackerrank.com/challenges/between-two-sets
+
+You have two arrays of integers. Determine all integers that satisfy the following two conditions:
+* The elements of the first array are all factors of the integer being considered (slice a)
+* The integer being considered is a factor of all elements of the second array (slice b)
+
+These numbers are referred to as being between the two arrays.
+You must determine how many such numbers exist.
+
+*/
+
+package between_two_sets
+
+// factorsOf checks if all numbers in list are factors of t 
+func factorsOf(ls []int32, t int32) bool {
+    for _, n := range ls {
+        if t % n != 0 {
+            return false
+        }
+    }
+    return true
+}
+
+// isFactor checks if t is factor of all numbers in list
+func isFactor(ls []int32, t int32) bool {
+    for _, n := range ls {
+        if n % t != 0 {
+            return false
+        }
+    }
+    return true
+}
+
+// GetTotal checks total number of integers between 2 slices
+func GetTotal(a []int32, b []int32) int32 {
+    var res int32 = 0
+    for i := a[len(a)-1]; i <=  b[0]; i++ {
+        if factorsOf(a, i) && isFactor(b, i) {
+            res +=1 
+        }
+    }
+    return res
+}
---
+++ GO/HACKERRANK/between_two_sets_test.go
@@ -0,0 +1,33 @@
+package between_two_sets
+
+import (
+	"testing"
+)
+
+func TestGetTotal(t *testing.T) {
+	tests := []struct{
+		desc string
+		a []int32
+		b []int32
+		want int32
+	}{
+		{
+			desc: "success",
+			a: []int32{2,4},
+			b: []int32{16,32,96},
+			want: int32(3),
+		},
+		{
+			desc: "success",
+			a: []int32{3,4},
+			b: []int32{24,48},
+			want: int32(2),
+		},
+	}
+
+	for i, tc := range tests {
+		if got := GetTotal(tc.a, tc.b); got != tc.want {
+			t.Errorf("TestGetTotal(%d): %s: unexpected result got: %v, want: %v", i, tc.desc, got, tc.want)
+		}
+	}
+}
