db21fe864d5fe52bab4844d1fd261b403934f758
atugai <anton.tugai@gmail.com>
2018-09-20 16:12:16 +0100

    [Go][hackerrank.com] Drawing Book excercise

---
+++ GO/HACKERRANK/drawing_book.go
@@ -0,0 +1,49 @@
+/*
+
+[hackerrank.com] Drawing Book
+https://www.hackerrank.com/challenges/drawing-book
+
+*/
+
+/*
+
+Challenge here is to find way how to represent number of turns from front and
+from back of the book:
+
+Turns F | Pages | Turns B
+=========================
+      0 |   ' 1 | 4
+      1 | 2 ' 3 | 3
+      2 | 4 ' 5 | 2
+      3 | 6 ' 7 | 1
+      4 | 8 '   | 0
+
+*/
+
+package drawing_book
+
+// turnFront helper func to find number of turns from front
+func turnFront(p int32) int32 {
+	if p % 2 == 0 {
+		return p/2
+	} else {
+		return (p - 1)/2
+	}
+}
+
+// turnBack helper func to find number of turns from back
+func turnBack(n int32, p int32) int32 {
+	if p % 2 == 0 {
+		return (n - p)/2
+	} else {
+		return (n - p + 1)/2
+	}
+}
+
+// PageCount returns minimum number of page turns to get required page
+func PageCount(n int32, p int32) int32 {
+	if turnFront(p) > turnBack(n, p) {
+		return turnBack(n, p)
+	}
+	return turnFront(p)
+}
---
+++ GO/HACKERRANK/drawing_book_test.go
@@ -0,0 +1,33 @@
+package drawing_book
+
+import (
+	"testing"
+)
+
+func TestPageCount(t *testing.T) {
+	tests := []struct{
+		desc string
+		n int32
+		p int32
+		want int32
+	}{
+		{
+			"1st page from begin",
+			6,
+			2,
+			1,
+		},
+		{
+			"0 page from end ",
+			5,
+			4,
+			0,
+		},
+	}
+
+	for i, tc := range tests {
+		if got := PageCount(tc.n, tc.p); got != tc.want {
+			t.Errorf("PageCount(%d): %s: unexpected result got: %v, want: %v", i, tc.desc, got, tc.want)
+		}
+	}
+}
