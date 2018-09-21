0180b0830b0fd4c9e7d7f97b8ef1a9b36185c870
atugai <anton.tugai@gmail.com>
2018-09-21 13:02:14 +0100

    [Go][hackerrank.com] Electronics Shop refactoring

--- GO/HACKERRANK/electronics_shop.go
+++ GO/HACKERRANK/electronics_shop.go
@@ -10,19 +10,16 @@ package electronics_shop
 // GetMoneySpent calculates maximum spendable budget for kb and drive
 func GetMoneySpent(keyboards []int32, drives []int32, b int32) int32 {
 	var (
-		outcomes []int32
 		res int32
 		found bool
 	)
 	for _, kb := range keyboards {
 		for _, dr := range drives {
-			outcomes = append(outcomes, kb + dr)
-		}
-	}
-	for _, r := range outcomes {
-		if r <= b && r >= res {
-			found = true
-			res = r
+			r := kb + dr
+			if r <= b && r >= res {
+				found = true
+				res = r
+			}
 		}
 	}
 	if found {
