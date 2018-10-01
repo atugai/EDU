0db3c72c019a8125beee9a53a423469eda8e8b50
atugai <anton.tugai@gmail.com>
2018-10-01 18:01:46 +0100

    [Go][hackerrank.com] Circular Array Rotation excercise

---
+++ GO/HACKERRANK/circular_array_rotation.go
@@ -0,0 +1,29 @@
+/*
+
+[hackerrank.com] Circular Array Rotation
+https://www.hackerrank.com/challenges/circular-array-rotation
+
+*/
+
+/*
+
+This implementation is not mamory efficient as requires to store modified slice
+TODO: optimize
+
+*/
+
+package circular_array_rotation
+
+// CircularArrayRotation calculates new array element positions after right circullar rotation
+func CircularArrayRotation(a []int32, k int32, queries []int32) []int32 {
+	mod := make([]int32, len(a))
+		for i, v := range a {
+			mod[(i + int(k)) % len(a)] = v
+		}
+
+	res := make([]int32, len(queries))
+	for i, v := range queries {
+		res[i] = mod[v]
+	}
+	return res
+}
---
+++ GO/HACKERRANK/circular_array_rotation_test.go
@@ -0,0 +1,30 @@
+package circular_array_rotation
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCircularArrayRotation(t *testing.T) {
+	tests := []struct{
+		desc string
+		slice []int32
+		steps int32
+		queries []int32
+		want []int32
+	}{
+		{
+			"success",
+			[]int32{10, 20, 30},
+			2,
+			[]int32{0, 1, 2},
+			[]int32{20, 30, 10},
+		},
+	}
+
+	for i, tc := range tests {
+		if got := CircularArrayRotation(tc.slice, tc.steps, tc.queries); !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("TestCircularArrayRotation(%d): %s: unexpected result got: %v, want: %v", i, tc.desc, got, tc.want)
+		}
+	}
+}
