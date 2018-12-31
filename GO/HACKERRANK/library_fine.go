ee4e23477a5dcfed0989b901715a27e82bed58bd
atugai <anton.tugai@gmail.com>
2018-12-31 20:08:45 +0000

    [Go][hackerrank.com] Library Fine

---
+++ GO/HACKERRANK/library_fine.go
@@ -0,0 +1,28 @@
+/*
+  
+[hackerrank.com] Library Fine
+https://www.hackerrank.com/challenges/library-fine
+
+*/
+
+package library_fine
+
+// LibraryFine calculates fee to ba paid for return book delay.
+func LibraryFine(d1 int32, m1 int32, y1 int32, d2 int32, m2 int32, y2 int32) int32 {
+	if y1 > y2 {
+		return 10000
+	}
+	if y1 < y2 {
+		return 0
+	}
+	if m1 > m2 {
+		return (m1 - m2) * 500
+	}
+	if m1 < m2 {
+		return 0
+	}
+	if d1 > d2 {
+		return (d1 - d2) * 15
+	}
+	return 0
+}
---
+++ GO/HACKERRANK/library_fine_test.go
@@ -0,0 +1,45 @@
+package library_fine
+
+import (
+	"testing"
+)
+
+func TestLibraryFine(t *testing.T) {
+	tests := []struct{
+		desc string
+		rY int32
+		rM int32
+		rD int32
+		eY int32
+		eM int32
+		eD int32
+		want int32
+	}{
+		{
+			desc: "no fine",
+			rY: 2015,
+			rM: 6,
+			rD: 6,
+			eY: 2016,
+			eM: 6,
+			eD: 9,
+			want: 0,
+		},
+		{
+			desc: "3 days delay",
+			rY: 2015,
+			rM: 6,
+			rD: 9,
+			eY: 2015,
+			eM: 6,
+			eD: 6,
+			want: 45,
+		},
+	}
+
+	for i, tc := range tests {
+		if got := LibraryFine(tc.rD, tc.rM, tc.rY, tc.eD, tc.eM, tc.eY); got != tc.want {
+			t.Errorf("LibraryFine(%d): %s: unexpected result got: %v, want: %v", i, tc.desc, got, tc.want)
+		}
+	}
+}
