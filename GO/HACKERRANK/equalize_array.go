3f00108190f9511399aff46a4d040d91d2a82019
atugai <anton.tugai@gmail.com>
2019-01-03 19:01:29 +0000

    [Go][hackerrank.com] Equalize the Array

---
+++ GO/HACKERRANK/equalize_array.go
@@ -0,0 +1,36 @@
+/*
+
+[hackerrank.com] Equalize the Array
+https://www.hackerrank.com/challenges/equality-in-a-array
+
+*/
+
+package equalize_array
+
+import (
+	"sort"
+)
+
+// EqualizeArray calculates minimum number of lelements to remove from arra to leave only equal elements.
+func EqualizeArray(arr []int32) int32 {
+	els := map[int32]int{}
+	for _, v := range arr {
+		if _, ok := els[v]; !ok {
+			els[v] = 1
+		} else {
+			els[v] += 1
+		}
+	}
+
+	nums := []int{}
+	for _, v := range els {
+		nums = append(nums, v)
+	}
+	sort.Ints(nums)
+
+	var res int32 = 0
+	for i := 0; i < len(nums) - 1; i++ {
+		res += int32(nums[i])
+	}
+	return res
+}
---
+++ GO/HACKERRANK/equalize_array_test.go
@@ -0,0 +1,29 @@
+package equalize_array
+
+import (
+	"testing"
+)
+
+func TestEqualizeArray(t *testing.T) {
+	tests := []struct{
+		desc string
+		arr []int32
+		want int32
+	}{
+		{
+			desc: "success",
+			arr: []int32{3, 3, 2, 1, 3},
+			want: 2,
+		}, {
+			desc: "success",
+			arr: []int32{1, 2, 3, 1, 2, 3, 3, 3},
+			want: 4,
+		},
+	}
+
+        for i, tc := range tests {
+                if got := EqualizeArray(tc.arr); got != tc.want {
+                        t.Errorf("EqualizeArray(%d): %s: unexpected result got: %v, want: %v", i, tc.desc, got, tc.want)
+                }
+        }
+}
