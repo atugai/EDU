6d0b5c897c4e8926f2a57b08351623c910cf3adc
atugai <anton.tugai@gmail.com>
2019-01-04 15:42:25 +0000

    [Go][hackerrank.com] ACM ICPC Team

---
+++ GO/HACKERRANK/acm_icpc_team.go
@@ -0,0 +1,55 @@
+/*
+  
+[hackerrank.com] ACM ICPC Team
+https://www.hackerrank.com/challenges/acm-icpc-team
+
+*/
+
+package acm_icpc_team
+
+import (
+	"sort"
+)
+
+// AcmTeam calculates maximum number of teams can cover maximum number of topics.
+func AcmTeam(topic []string) []int32 {
+	ts := [][]int{}
+	for _, v := range topic {
+		tempRes := []int{}
+		for _, d := range v {
+			if d == '0' {
+				tempRes = append(tempRes, 0)
+			} else {
+				tempRes = append(tempRes, 1)
+			}
+		}
+		ts = append(ts, tempRes)
+	}
+	
+	resMap := map[int32]int32{}
+	for i := 0; i < len(ts); i++ {
+		for j := i + 1; j < len(ts); j++ {
+			var teamScore int32 = 0
+			for k, _ := range ts[i] {
+				if ts[i][k] == 1 || ts[j][k] == 1 {
+					teamScore += 1
+				}
+			}
+
+			if _, ok := resMap[teamScore]; !ok {
+				resMap[teamScore] = 1
+			} else {
+				resMap[teamScore] += 1
+			}
+		}
+	}
+	
+	scores := []int{}
+	for k := range resMap {
+		scores = append(scores, int(k))
+	}
+
+	sort.Ints(scores)
+	maxScore := int32(scores[len(scores)-1])
+	return []int32{maxScore, resMap[maxScore]}
+}
---
+++ GO/HACKERRANK/acm_icpc_team_test.go
@@ -0,0 +1,31 @@
+package acm_icpc_team
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestAcmTeam(t *testing.T) {
+	tests := []struct{
+		desc string
+		in []string
+		want []int32
+	}{
+		{
+			desc: "success",
+			in: []string{"10101", "11100", "11010", "00101"},
+			want: []int32{5, 2},
+		}, {
+			desc: "success",
+			in: []string{"11101", "10101","11001", "10111", "10000", "01110"},
+			want: []int32{5, 6},
+
+		},
+	}
+
+        for i, tc := range tests {
+                if got := AcmTeam(tc.in); !reflect.DeepEqual(got, tc.want) {
+                        t.Errorf("AcmTeam(%d): %s: unexpected result got: %v, want: %v", i, tc.desc, got, tc.want)
+                }
+        }
+}
