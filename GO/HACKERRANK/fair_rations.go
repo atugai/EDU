8b13c6a76a84cf68b17d6b7bf7a9bfa0f533ab09
atugai <anton.tugai@gmail.com>
2019-12-29 22:16:58 +0000

    [Go][hackerrank.com] Fair Rations

---
+++ GO/HACKERRANK/fair_rations.go
@@ -0,0 +1,37 @@
+/*
+
+[hackerrank.com] Fair Rations
+https://www.hackerrank.com/challenges/fair-rations
+
+*/
+
+package fair_rations
+
+// Returns total number of rations required to make all allocations even. If
+// even allocation is not possible returs -1.
+func FairRations(B []int32) int32 {
+	// Numbers should be increased only between odd digit pairs, find all odds.
+	var pos []int
+	for i, v := range B {
+		if v % 2 != 0 {
+			pos = append(pos, i)
+		}
+	}
+
+	// If number of odd numbers is not even then we can't allocate even number of
+	// rations.
+	if len(pos) % 2 != 0 {
+		return -1
+	}
+
+	// Walk through all odd pairs and increase reations allocation.
+	var res int32 = 0
+	for i := 0; i < len(pos); i += 2 {
+		beg := pos[i]
+		end := pos[i+1]
+		for j := beg; j < end; j++ {
+			res += 2
+		}
+	}
+	return res
+}
---
+++ GO/HACKERRANK/fair_rations_test.go
@@ -0,0 +1,24 @@
+package fair_rations
+
+import "testing"
+
+func TestFairRations(t *testing.T) {
+	tests := []struct{
+		in []int32
+		want int32
+	}{
+		{
+			in: []int32{2, 3, 4, 5, 6},
+			want: 4,
+		},{
+			in: []int32{1, 2},
+			want: -1,
+		},
+	}
+
+	for i, tc := range tests {
+		if got := FairRations(tc.in); got != tc.want {
+			t.Errorf("TestFairRations(%d): unexpected result got: %v, want: %v", i, got, tc.want)
+		}
+	}
+}
